d7db5559e2bb3ec9bc641ffc5708e6a371fc8828
WesRamox <wesleyramox258@gmail.com>
2025-09-01 22:58:41 -0300

    feat: setup initial frontend structure

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,14 @@
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export interface Product {
+  id: number;
+  name: string;
+  description?: string;
+  price: number;
+  stock: number;
+  categoryId: number;
+  categoryName?: string;
+}
