43d6cf1e4ec5eebecaef670b78b3954eba11e2d8
WesRamox <wesleyramox258@gmail.com>
2025-09-01 22:25:44 -0300

    feat: create ProductValidator and define Product and Category entities

--- api/src/Hypesoft.Domain/Entities/Product.cs
+++ api/src/Hypesoft.Domain/Entities/Product.cs
@@ -3,10 +3,13 @@ namespace Hypesoft.Domain.Entities
   public class Product
   {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public decimal Price { get; set; }
-    public string Category { get; set; }
     public int Stock { get; set; }
+
+    // Novo campo para categoria
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; } = string.Empty;
   }
 }
