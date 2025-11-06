34f3436a1cd23880cb86d5664932979c5bfede45
EduardGuilherme <eduguilherme48@gmail.com>
2025-11-06 20:37:08 -0300

    feat(api): add controllers from Api

---
+++ api/src/Hypesoft.API/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using Hypesoft.Application.Dtos;
+using Hypesoft.Application.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hypesoft.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _service;
+
+        public CategoryController(ICategoryService service)
+        {
+            _service = service;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories() => Ok(await _service.GetAllAsync());
+        
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoryById(string id)
+        {
+            var category = await _service.GetByIdAsync(id);
+            return category is not null ? Ok(category) : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory([FromBody] CategoryDto dto)
+        {
+            var createdCategory = await _service.AddAsync(dto); 
+            return CreatedAtAction(nameof(GetCategoryById), new { id = createdCategory.Id }, createdCategory);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            var existingCategory = await _service.GetByIdAsync(id);
+            if (existingCategory is null)
+            {
+                return NotFound();
+            }
+            await _service.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ api/src/Hypesoft.API/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using Hypesoft.Application.Dtos;
+using Hypesoft.Application.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hypesoft.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _service;
+
+        public ProductController(IProductService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts() => Ok(await _service.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(string id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            return product is not null ? Ok(product) : NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] ProductDto dto)
+        {
+            var newProduct = await _service.AddAsync(dto);
+            return CreatedAtAction(nameof(GetProductById), new { id = newProduct.Id }, newProduct);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(string id, [FromBody] Hypesoft.Application.Dtos.ProductDto dto)
+        {
+            var existingProduct = await _service.GetByIdAsync(id);
+            if (existingProduct is null)
+            {
+                return NotFound();
+            }
+            await _service.UpdateAsync(id, dto);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            var existingProduct = await _service.GetByIdAsync(id);
+            if (existingProduct is null)
+            {
+                return NotFound();
+            }
+            await _service.DeleteAsync(id);
+            return NoContent();
+        }
+
+    }
+}
