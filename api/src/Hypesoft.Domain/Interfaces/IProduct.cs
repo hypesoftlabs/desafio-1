25217a405d49b17586034ff1b9dbb626820b9f45
EduardGuilherme <eduguilherme48@gmail.com>
2025-11-06 18:17:13 -0300

    feat(interfaces): add interfaces IProduct and ICategory

---
+++ api/src/Hypesoft.Domain/Interfaces/IProduct.cs
@@ -0,0 +1,18 @@
+﻿using Hypesoft.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypesoft.Domain.Interfaces
+{
+    public interface IProduct
+    {
+        Task<List<Product>> GetAllAsync();
+        Task<Product?> GetByIdAsync(string id);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(string id);
+    }
+}
