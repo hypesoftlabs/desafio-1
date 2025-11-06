35290358a7a87a322e8bbe1ec392c12d7dcd4ce9
EduardGuilherme <eduguilherme48@gmail.com>
2025-11-06 20:38:10 -0300

    feat(application): add DTO products

---
+++ api/src/Hypesoft.Application/Dtos/ProductDto.cs
@@ -0,0 +1,21 @@
+﻿using Hypesoft.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypesoft.Application.Dtos
+{
+    public class ProductDto
+    {
+        public string? Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string? CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public int StockQuantity { get; set; }
+        public bool IsLowStock => StockQuantity < 10;
+    }
+}
