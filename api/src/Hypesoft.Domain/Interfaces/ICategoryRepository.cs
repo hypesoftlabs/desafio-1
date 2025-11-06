52fb65cec12c308abcbad4d4715be7a1eba34bd6
EduardGuilherme <eduguilherme48@gmail.com>
2025-11-06 20:41:27 -0300

    feat(application): Adds interfaces of repository from category

---
+++ api/src/Hypesoft.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Hypesoft.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypesoft.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetAllAsync();
+        Task<Category?> GetByIdAsync(string id);
+        Task AddAsync(Category category);
+        Task DeleteAsync(string id);
+    }
+}
