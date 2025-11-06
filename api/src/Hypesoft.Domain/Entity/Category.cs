c14330731bf541b0a77609c290c02bcf710a4855
EduardGuilherme <eduguilherme48@gmail.com>
2025-11-06 12:34:52 -0300

    initial

---
+++ api/src/Hypesoft.Domain/Entity/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypesoft.Domain.Entity
+{
+    public class Category
+    {
+    }
+}
