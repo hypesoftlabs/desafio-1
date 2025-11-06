1f05c71bb19dbc413ed823d59fbaf764a80bb52b
EduardGuilherme <eduguilherme48@gmail.com>
2025-11-06 18:09:23 -0300

    feat(Config): add configs

---
+++ api/src/Hypesoft.Infrastructure/Config/MongoDbSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypesoft.Infrastructure.Config
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+    }
+}
