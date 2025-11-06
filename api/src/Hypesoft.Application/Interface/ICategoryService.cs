8817016f004793ac3807326e4a5472ab20ca0dc9
EduardGuilherme <eduguilherme48@gmail.com>
2025-11-06 20:39:49 -0300

    feat(application): Adds application layer interfaces

---
+++ api/src/Hypesoft.Application/Interface/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using Hypesoft.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypesoft.Application.Interface
+{
+    public interface ICategoryService
+    {
+        Task<List<CategoryDto>> GetAllAsync();
+        Task<CategoryDto?> GetByIdAsync(string id);
+        Task<CategoryDto> AddAsync(CategoryDto dto);
+        Task DeleteAsync(string id);
+    }
+}
---
+++ api/src/Hypesoft.Application/Interface/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using Hypesoft.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypesoft.Application.Interface
+{
+    public interface IProductService
+    {
+        Task<List<ProductDto>> GetAllAsync();
+        Task<ProductDto?> GetByIdAsync(string id);
+        Task<ProductDto> AddAsync(ProductDto dto);
+        Task UpdateAsync(string id, ProductDto dto);
+        Task DeleteAsync(string id);
+    }
+}
