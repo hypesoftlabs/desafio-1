fc8658a75d7992f7ab8e15fe94c4566fe3b791bc
EduardGuilherme <eduguilherme48@gmail.com>
2025-11-07 18:18:37 -0300

    feat(api): update  ProductController

--- api/src/Hypesoft.API/Controllers/ProductController.cs
+++ api/src/Hypesoft.API/Controllers/ProductController.cs
@@ -1,5 +1,8 @@
-﻿using Hypesoft.Application.Dtos;
+﻿using Hypesoft.Application.Commands.ProductsCommand;
+using Hypesoft.Application.Dtos;
 using Hypesoft.Application.Interface;
+using Hypesoft.Application.Queries;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hypesoft.API.Controllers
@@ -8,48 +11,57 @@ namespace Hypesoft.API.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        private readonly IProductService _service;
+        /*private readonly IProductService _service;
 
         public ProductController(IProductService service)
         {
             _service = service;
+        }*/
+
+        private readonly IMediator _mediator;
+        public ProductController(IMediator mediator)
+        {
+            _mediator = mediator;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllProducts() => Ok(await _service.GetAllAsync());
+        public async Task<IActionResult> GetAllProducts() {
+            var query = new GetAllProductQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetProductById(string id)
         {
-            var product = await _service.GetByIdAsync(id);
-            return product is not null ? Ok(product) : NotFound();
+            var query = new GetProductsIdQuery {Id = id };
+            var result = await _mediator.Send(query);
+
+            if (result is null)
+                return NotFound(new { message = $"Produto com ID {id} não encontrado." });
+
+            return Ok(result);
         }
         [HttpPost]
-        public async Task<IActionResult> CreateProduct([FromBody] ProductDto dto)
+        public async Task<IActionResult> CreateProduct([FromBody] CreateProductCommand command)
         {
-            var newProduct = await _service.AddAsync(dto);
-            return CreatedAtAction(nameof(GetProductById), new { id = newProduct.Id }, newProduct); 
+            var result = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetProductById), new { id = result.Id }, result);
         }
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateProduct(string id, [FromBody] Hypesoft.Application.Dtos.ProductDto dto)
+        public async Task<IActionResult> UpdateProduct(string id, [FromBody] UpdateProductCommand command)
         {
-            var existingProduct = await _service.GetByIdAsync(id);
-            if (existingProduct is null)
-            {
-                return NotFound();
-            }
-            await _service.UpdateAsync(id, dto);
-            return NoContent();
+            if (id != command.Id)
+                return BadRequest(new { message = "O ID da URL e do corpo da requisição devem ser iguais." });
+
+            var result = await _mediator.Send(command);
+            return Ok(result);
         }
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteProduct(string id)
         {
-            var existingProduct = await _service.GetByIdAsync(id);
-            if (existingProduct is null)
-            {
-                return NotFound();
-            }
-            await _service.DeleteAsync(id);
+            var command = new DeleteProductCommand { Id = id };
+            await _mediator.Send(command);
             return NoContent();
         }
 
