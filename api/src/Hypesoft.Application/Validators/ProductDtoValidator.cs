c9a39e00ae33f36e86c51894ebdf9376bcae4947
EduardGuilherme <eduguilherme48@gmail.com>
2025-11-07 13:37:01 -0300

    feat(Validators): add Validators

---
+++ api/src/Hypesoft.Application/Validators/ProductDtoValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Hypesoft.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypesoft.Application.Validators
+{
+    public class ProductDtoValidator : AbstractValidator<ProductDto>
+    {
+        public ProductDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("O nome do produto é obrigatório.")
+                .MaximumLength(100).WithMessage("O nome não pode ultrapassar 100 caracteres.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("A descrição é obrigatória.");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("O preço deve ser maior que zero.");
+
+            RuleFor(x => x.CategoryId)
+                .NotEmpty().WithMessage("O ID da categoria é obrigatório.");
+
+            RuleFor(x => x.StockQuantity)
+                .GreaterThanOrEqualTo(0).WithMessage("A quantidade em estoque não pode ser negativa.");
+        }
+    }
+}
